248a491f273bbdf969e8fda73a3ede630dc33ce9
Jeffrey Saeteros <js11692@nyu.edu>
2023-03-27 13:26:50 -0400

    php basics

--- day_15_16_PHP_introduction/00_EmptyExample.php
+++ day_15_16_PHP_introduction/00_EmptyExample.php
@@ -7,6 +7,30 @@
 
   <body>
     <h1>PHP & Server-Side Programming</h1>
+
+    <?php
+      error_reporting(E_ALL);
+      ini_set("display erros", 1);
+
+      $var1 = 'hello';
+      $var2 = 5;
+      $var3 = 5.7;
+      $var4 = true;
+
+      // casting in php
+      $var5 = '12345';
+      $var6 = intval($var5);
+      $var7 = (int) $var5;
+
+      print $var1;
+      print "<br>";
+      print $var2;
+      print "<br>";
+      print $var3;
+      print "<br>";
+      print $var4;
+      print "<br>";
+    ?>
     
   </body>
 </html>
